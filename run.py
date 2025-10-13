8e6c86f39d14664b7ae5263a1a6d14c8b47da1a4
jwillianteo <167312337+jwillianteo@users.noreply.github.com>
2025-10-13 20:53:29 -0300

    Prepara aplicação para produção com Flask-Migrate e build script

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
 # run.py
-
-from app import create_app, db
-
-# Cria a instância da nossa aplicação
-app = create_app()
-
 if __name__ == '__main__':
-    # Este bloco garante que o banco de dados e as tabelas
-    # sejam criados antes da primeira requisição, se não existirem.
     with app.app_context():
         db.create_all()
-    
-    # Inicia o servidor de desenvolvimento
-    app.run(debug=True)
+    # Altere esta linha
+    app.run(debug=False)
